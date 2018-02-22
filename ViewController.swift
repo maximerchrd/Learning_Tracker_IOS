ffd2de454338dd14291ea17615909d6ec89819a7
maximerchrd <maximerchrd@gmail.com>
2018-02-23 00:08:27 +0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Learning_Tracker_IOS
+//
+//  Created by Maxime Richard on 22.02.18.
+//  Copyright © 2018 Maxime Richard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var launchInteractiveMode: UIButton!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
