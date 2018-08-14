e2568bc9b01a860b1c0825440f4ba94ba1771663
maximerchrd <maximerchrd@gmail.com>
2018-08-14 23:43:46 +0400

    removed mailing of logs for submitting

--- ViewControllers/SettingsViewController.swift
+++ ViewControllers/SettingsViewController.swift
@@ -84,61 +84,6 @@ class SettingsViewController: UIViewController, MFMailComposeViewControllerDeleg
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func sendMail(_ sender: Any) {
-        let mailComposeViewController = configureMailController()
-        if MFMailComposeViewController.canSendMail() {
-            self.present(mailComposeViewController, animated: true, completion: nil)
-        } else {
-            showMailError()
-        }
-    }
-    
-    func configureMailController() -> MFMailComposeViewController {
-        let mailComposerVC = MFMailComposeViewController()
-        mailComposerVC.mailComposeDelegate = self
-        
-        mailComposerVC.setToRecipients(["learning.tracker.dev@gmail.com"])
-        mailComposerVC.setSubject(modelIdentifier())
-        
-        do {
-            logs = try DbTableLogs.getNotSentLogs();
-        } catch let error {
-            print(error)
-        }
-        var fullLog = ""
-        for log in logs {
-            fullLog = fullLog + log + "\n"
-        }
-        mailComposerVC.setMessageBody(fullLog, isHTML: false)
-        
-        return mailComposerVC
-    }
-    
-    func modelIdentifier() -> String {
-        if let simulatorModelIdentifier = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] { return simulatorModelIdentifier }
-        var sysinfo = utsname()
-        uname(&sysinfo) // ignore return value
-        return String(bytes: Data(bytes: &sysinfo.machine, count: Int(_SYS_NAMELEN)), encoding: .ascii)!.trimmingCharacters(in: .controlCharacters)
-    }
-    
-    func showMailError() {
-        let sendMailErrorAlert = UIAlertController(title: "Could not send email", message: "Your device could not send email", preferredStyle: .alert)
-        let dismiss = UIAlertAction(title: "Ok", style: .default, handler: nil)
-        sendMailErrorAlert.addAction(dismiss)
-        self.present(sendMailErrorAlert, animated: true, completion: nil)
-    }
-    
-    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-        controller.dismiss(animated: true, completion: nil)
-        if result.rawValue == MFMailComposeResult.sent.rawValue {
-            do {
-                try DbTableLogs.deleteLog()
-            } catch let error {
-                print(error)
-            }
-        }
-    }
-    
     //function enabling dismissing of keyboard when return pressed
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
